8c698ab8481d5e39f7e29a048cc0242184b3d4c7
Maciek Makowski <maciek.makowski@gmail.com>
2010-10-17 16:28:07 +0100

    made status code a parameter

--- modules/html.py
+++ modules/html.py
@@ -4,10 +4,10 @@ Provides functions and templates for generating HTML reponses.
 import httplib
 
 __author__ = 'Maciek Makowski'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
-def send_html_response(http, title, body):
-    http.send_response(httplib.OK)
+def send_html_response(http, title, body, status=httplib.OK):
+    http.send_response(status)
     http.send_header('Content-type', 'text/html')
     http.end_headers()
     http.wfile.write(page_template % (title, body))
