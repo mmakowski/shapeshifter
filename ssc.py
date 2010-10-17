74825e46977f2e58497edd004914850ea9f34aa6
Maciek Makowski <maciek.makowski@gmail.com>
2010-10-17 18:50:30 +0100

    factored out request()

--- ssc.py
+++ ssc.py
@@ -1,13 +1,20 @@
+'''
+An utility which issues requests to remote Shapeshifter instances.
+'''
+
 import httplib
 import mimetypes
+import os
 
-port = 5457
+default_port = 5457
 
-def encode_multipart_formdata(fields, files):
+def _encode_multipart_formdata(fields, files):
     """
     fields is a sequence of (name, value) elements for regular form fields.
     files is a sequence of (name, filename, value) elements for data to be uploaded as files
     Return (content_type, body) ready for httplib.HTTP instance
+
+    Taken from http://code.activestate.com/recipes/146306/
     """
     BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
     CRLF = '\r\n'
@@ -20,7 +27,7 @@ def encode_multipart_formdata(fields, files):
     for (key, filename, value) in files:
         L.append('--' + BOUNDARY)
         L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
-        L.append('Content-Type: %s' % get_content_type(filename))
+        L.append('Content-Type: %s' % _get_content_type(filename))
         L.append('')
         L.append(value)
     L.append('--' + BOUNDARY + '--')
@@ -29,15 +36,36 @@ def encode_multipart_formdata(fields, files):
     content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
     return content_type, body
 
-def get_content_type(filename):
+def _get_content_type(filename):
     return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
 
 
-conn = httplib.HTTPConnection('localhost', port)
-f = open('ssctrl.py')
-content_type, form_data = encode_multipart_formdata([], [('module_text', 'test', f.read())])
-f.close()
-conn.request('PUT', '/ssmodule/ssc', form_data, {'Content-type': content_type})
-resp = conn.getresponse()
-print resp.status, resp.reason
-print resp.read()
+def _print_usage():
+    print 'ssc targets URI method [mappings]'
+
+
+def _file_content(path):
+    with open(path) as f: return f.read()
+
+
+def request(targets, uri, method='POST', properties={}, files={}):
+    file_data = [(id, os.path.basename(path), _file_content(path)) for (id, path) in files.items()]
+    content_type, form_data = _encode_multipart_formdata(properties.items(), file_data)
+    responses = dict()
+    for target in targets:
+        conn = httplib.HTTPConnection(target)
+        conn.request(method, uri, form_data, {'Content-type': content_type})
+        responses[target] = conn.getresponse()
+    return responses
+    
+
+def main():
+    responses = request(['localhost:%s' % default_port], '/ssmodule/ssc', method='PUT', files={'module': 'ssc.py'})
+    resp = responses.values()[0]
+    print resp.status, resp.reason
+    print resp.read()
+
+
+if __name__ == '__main__':
+    main()
+
